f312b85fc464476998e05c593d673d6b05bfb036
ecrinyildiz <ecrin_3@hotmail.com>
2025-07-25 16:37:25 +0300

    refactor

--- modules/Simulation/src/SignalSolver.cpp
+++ modules/Simulation/src/SignalSolver.cpp
@@ -8,6 +8,7 @@ SignalSolver::SignalSolver(std::shared_ptr<SimulationScene> scene)
 
 std::shared_ptr<math::PointCloud> SignalSolver::solve()
 {
+
     // std::cout << "solving..." << std::endl;
     auto result = std::make_shared<math::PointCloud>();
     auto allPoints = scene_->getMergedPointCloud();
@@ -15,13 +16,22 @@ std::shared_ptr<math::PointCloud> SignalSolver::solve()
     const auto &txs = scene_->getTransmitters();
     const auto &rxs = scene_->getReceivers();
 
+    size_t tx_size = txs->size();
+    size_t rx_size = rxs->size();
+    std::vector<std::vector<float>> ToFVals(tx_size, std::vector<float>(rx_size, 0.0f));
+
+    int tx_no=-1;
     for (const auto &tx : txs)
     {
+        tx_no++;
+        int rx_no = -1;
         for (const auto &rx : rxs)
         {
+            rx_no++;
+
             // Filter points inside both FOVs
             auto inTxFov = tx->pointsInFov(*allPoints);
-
+ 
             if (!inTxFov || inTxFov->size() == 0)
             {
                 continue;
@@ -51,11 +61,29 @@ std::shared_ptr<math::PointCloud> SignalSolver::solve()
                     closest = pt;
                 }
             }
+            ToFVals[tx_no][rx_no] = minDist;
             // std::cout << solveCount_ << " " << tx->getName() << "-" << rx->getName() << "\t" << closest.toString() << "\t tof:" << minDist << std::endl;
             solveCount_++;
             result->addPoint(closest);
         }
     }
+    //return ToFVals
+
+
+    // seperate the func
+    // std::shared_ptr<math::PointCloud> SignalSolver::TofVals2AdsilPoints(const std::vector<std::vector<float>> &ToFVals)
+    auto result = std::make_shared<math::PointCloud>();
+
+
+
+
 
     return result;
 }
+
+
+{
+    
+
+}
+
